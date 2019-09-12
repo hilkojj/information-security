ba2f20d6928431bbd8816388d53f05f633567d88
hilkojj <hilkojj@outlook.com>
2019-09-12 15:48:56 +0200

    decrypted the text

--- src/main2.cpp
+++ src/main2.cpp
@@ -16,13 +16,12 @@ double sum(std::map<char, int> a)
 {
     int total = 0;
     for (auto x : a) total += x.second;
-    // std::cout << total << "\n\n";
     return total;
 }
 
 double standard_deviation(std::map<char, int> x)
 {
-    // return std::sqrt( sum((x * x)) / 26.0 - std::pow( sum(x) / 26.0, 2) );
+    return std::sqrt( sum((x * x)) / 26.0 - std::pow( sum(x) / 26.0, 2) );
     double length = x.size();
     double E = 0;
     double Ravg = sum(x) / length;
@@ -39,8 +38,6 @@ void magic(std::string data)
         double sum = 0;
         for (int i = 0; i < block_size; i++)
         {
-            if (i % block_size != 0) continue;
-
             std::map<char, int> fr;
             
             for(int j = i * block_size; j < (i * block_size) + block_size; j++)
@@ -50,7 +47,7 @@ void magic(std::string data)
             sum += standard_deviation(fr);
         }
 
-        std::cout << block_size << " " << sum << std::endl;
+        std::cout << block_size << " " << sum * data.length() / double(block_size) << std::endl;
     }
 }
 
@@ -60,4 +57,50 @@ int main()
     std::string data((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
     magic(data);
+
+    int block_size = 9;
+
+    auto fr = new std::map<char, int>[block_size];
+
+    for (int i = 0; i < data.length(); i += block_size)
+    {
+        for (int j = 0; j < block_size; j++)
+            fr[j][data[i + j]]++;
+    }
+    
+
+    std::string key;
+    for (int j = 0; j < block_size; j++)
+    {
+        auto &freq = fr[j];
+
+        char maxChar; // f
+        int maxFreq = 0;
+
+        for (auto x : freq) if (x.second > maxFreq)
+        {
+            maxFreq = x.second;
+            maxChar = x.first;
+        }
+
+        char keyChar = 'a' + ((maxChar - 'e' + 26) % 26);
+        key += keyChar;
+
+        std::cout << keyChar << " ";
+    }
+    std::cout << "\n";
+
+
+    std::cout << "\n\n\n----------------\n\n\n";
+
+    int i = -1;
+    while (data[++i])
+    {
+        char keyChar = key[i % block_size];
+        char t = (data[i] - keyChar + 26) % 26 + 'a';
+        std::cout << t;
+    }
+    std::cout << "\n";
+
+    delete[] fr;
 }   
