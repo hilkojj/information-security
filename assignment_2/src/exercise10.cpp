ec826a8fbde10e9306fc5ef12e1cc9498a4a8a8b
timostrating <timostrating@home.nl>
2019-09-28 14:05:50 +0200

    cleaned up ex 10

--- assignment_2/src/exercise10.cpp
+++ assignment_2/src/exercise10.cpp
@@ -5,21 +5,6 @@
 
 typedef unsigned char byte;
 
-template<typename T, size_t n>
-void print_array(T const(& arr)[n])
-{
-	for (size_t i = 0; i < n; i++)
-		std::cout << arr[i] << ' ';
-}
-
-void print_hex(const std::string& s)
-{
-    std::cout << std::hex << std::setfill('0');
-    for(unsigned char c : s)
-        std::cout << std::setw(2) << static_cast<int>(c) << ' ';
-    std::cout << std::dec << '\n';
-}
-
 std::string RC4(byte text[], int L, byte key[], int N) 
 {
     std::string out;
@@ -37,33 +22,22 @@ std::string RC4(byte text[], int L, byte key[], int N)
     }
     int i = 0; j = 0;
 
-    // Pseudo-random generation-algoritme
-    // for (int index=300; index >= 0; index--)
-    // {
-        int index = 256;
-        int textIndex = -1 - index;
-        // std::cout << "\n" << index << " ";
-        while (++textIndex < L)
+    int index = 256;
+    int textIndex = -1 - index;
+    while (++textIndex < L)
+    {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        std::swap(S[i], S[j]);
+        byte K = S[(S[i] + S[j]) % 256];
+
+        if (textIndex >= 0)
         {
-            i = (i + 1) % 256;
-            j = (j + S[i]) % 256;
-            std::swap(S[i], S[j]);
-            byte K = S[(S[i] + S[j]) % 256];
-
-            if (textIndex >= 0)
-            {
-                // std::cout << K << '^' << text[textIndex] << " = ";
-                char c = char(text[textIndex] ^ K);
-                // std::cout << c << '\n';
-                out += c;
-            }
-
-            // std::coi
-
-            // if (textIndex >= 0) 
+            char c = char(text[textIndex] ^ K);
+            out += c;
         }
+    }
 
-    // }
     return out;
 }
 
@@ -71,7 +45,6 @@ int main(int argc, char *argv[])
 {
     // https://en.wikipedia.org/wiki/RC4#Test_vectors
     byte input_key[] = {'2', '0', '1', '9'};
-    // byte input_text[] = {'P', 'l', 'a', 'i', 'n', 't', 'e', 'x', 't'}; // "Plaintext"
 
     std::string input_text_str, line;
     while (std::getline(std::cin, line)) input_text_str += line + '\n';
@@ -79,10 +52,6 @@ int main(int argc, char *argv[])
     byte input_text[input_text_str.size()];
     int i = 0;
     for (auto &c : input_text_str) input_text[i++] = byte(c);
-    
-
-    std::string output_keystream = "EB9F7781B734CA72A719...";
-    std::string output_ciphertext = "BBF316E8D940AF0AD3";
 
     std::cout << RC4(input_text, input_text_str.size(), input_key, 4);
 }
